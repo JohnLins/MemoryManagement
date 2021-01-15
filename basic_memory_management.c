9659233a6c31eddc53814d0bde23327cb1da8bcc
John Lins <46113320+JohnLins@users.noreply.github.com>
2021-01-14 20:06:09 -0800

    Create basic_memory_management.c

---
+++ basic_memory_management.c
@@ -0,0 +1,23 @@
+//https://www.tutorialspoint.com/c_standard_library/c_function_free.htm
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main () {
+   char *str;
+
+   /* Initial memory allocation */
+   str = (char *) malloc(15);
+   strcpy(str, "tutorialspoint");
+   printf("String = %s,  Address = %u\n", str, str);
+
+   /* Reallocating memory */
+   str = (char *) realloc(str, 25);
+   strcat(str, ".com");
+   printf("String = %s,  Address = %u\n", str, str);
+
+   /* Deallocate allocated memory */
+   free(str);
+   
+   return(0);
+}
