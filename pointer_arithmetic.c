565af40803029d311b952a05faccb9bf444fc130
John Lins <46113320+JohnLins@users.noreply.github.com>
2021-03-03 22:17:58 -0800

    Create pointer_arithmetic.c

---
+++ pointer_arithmetic.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+
+int main () {
+
+   int  arr[] = {5, 7, 3};
+   int  *ptr;
+
+   
+   ptr = arr;
+  
+   
+
+      printf("address: %x\n", ptr);
+      
+      printf("value: %d\n", *ptr);
+      
+      ptr+=2;
+      
+      
+      printf("address: %x\n", ptr);
+      
+      printf("value: %d\n", *ptr);
+      
+      ptr--;
+      
+      
+      printf("address: %x\n", ptr);
+      
+      printf("value: %d\n", *ptr);
+   
+  
+   return 0;
+}
