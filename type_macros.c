dbddf5f4f6b81004ade2fc14c2d197f2b375e73e
John Lins <46113320+JohnLins@users.noreply.github.com>
2021-05-17 18:53:22 -0700

    Create type_macros.c

---
+++ type_macros.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define f64 double
+#define f32 float
+#define u8 unsigned short short int
+#define u16 unsigned short int
+#define u32 unsigned int
+#define u64 unsigned long int
+#define u128 unsigned long long int
+#define i8 short short int
+#define i16 short int 
+#define i32 int
+#define i64 long int
+#define i128 long long int
+
+int main()
+{
+    f64 x = 2.0;
+    printf("%f", x);
+
+    return 0;
+}
