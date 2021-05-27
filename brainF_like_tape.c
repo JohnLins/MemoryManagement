8994d210fb703789da6e28f7468f69eca06d5fdc
John Lins <46113320+JohnLins@users.noreply.github.com>
2021-05-26 19:04:05 -0700

    Update

--- brainF_like_tape.c
+++ brainF_like_tape.c
@@ -11,11 +11,37 @@ int *ptr = tape;
 #define OUT printf("%d\n", *ptr);
 
 
-
-
-
 int main(){
   UP; UP; ADD; ADD; SUB; OUT;
 
   return 0;
 }
+
+
+
+/*
+Or:
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+#define UP ptr += 1;
+#define DOWN ptr -= 1;
+#define ADD *ptr += 1;
+#define SUB *ptr -= 1;
+#define OUT printf("%d\n", *ptr);
+
+
+int main(){
+  int *tape = malloc(1000*sizeof(int));
+
+  int *ptr = tape;
+  
+  
+  UP; UP; ADD; ADD; SUB; OUT;
+
+  free(tape);
+  return 0;
+}
+*/
