882d70cc97221418ca645f62f0b4de415a780da3
John Lins <46113320+JohnLins@users.noreply.github.com>
2021-06-02 08:59:51 -0700

    Update array_queue.c

--- array_queue.c
+++ array_queue.c
@@ -3,44 +3,6 @@
 
 int QUEUE[MAX],front=-1,rear=-1;
 
-void remove_from_Q(int queue[]);
-void push(int queue[], int value)
-{
-    if(rear==-1)
-    {
-        front = rear = 0;
-        queue[rear] = value;
-    }
-    else if(rear==MAX-1)
-    {
-      pop(QUEUE);
-      queue[rear] = value;
-
-        return;
-    }
-    else
-    {
-        rear++;
-        queue[rear] = value;
-    }
-   
-}
-
-
-
-void display(int queue[])
-{   
-    printf("\nqueue: \n");
-    if(rear==-1) { 
-        printf("\nempty\n"); 
-        return; 
-    }
-    for(int i=front;i<=rear;i++)
-    { 
-        printf("%d,",queue[i]); 
-    }
-
-}
 
 
 void pop(int queue[])
@@ -63,6 +25,44 @@ void pop(int queue[])
     }
     printf("\nremoved %d.", value);
 }
+
+void push(int queue[], int value)
+{
+    if(rear==-1)
+    {
+        front = rear = 0;
+        queue[rear] = value;
+    }
+    else if(rear==MAX-1)
+    {
+      pop(QUEUE);
+      queue[rear] = value;
+
+        return;
+    }
+    else
+    {
+        rear++;
+        queue[rear] = value;
+    }
+   
+}
+
+void display(int queue[])
+{   
+    printf("\nqueue: \n");
+    if(rear==-1) { 
+        printf("\nempty\n"); 
+        return; 
+    }
+    for(int i=front;i<=rear;i++)
+    { 
+        printf("%d,",queue[i]); 
+    }
+
+}
+
+
 
 
 
